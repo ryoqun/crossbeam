6aa6a7d22716edc9f7c9eafc6a9662d4c31c64b7
Ryo Onodera <ryoqun@gmail.com>
2022-07-03 13:18:48 +0900

    save

--- crossbeam-deque/src/deque.rs
+++ crossbeam-deque/src/deque.rs
@@ -909,7 +909,7 @@ impl<T, C: CustomCollector> Stealer<T, C> {
         // If the current thread is already pinned (reentrantly), we must manually issue the
         // fence. Otherwise, the following pinning will issue the fence anyway, so we don't
         // have to.
-        if epoch::is_pinned::<epoch::DefaultCollector>() {
+        if epoch::is_pinned::<C>() {
             atomic::fence(Ordering::SeqCst);
         }
 
