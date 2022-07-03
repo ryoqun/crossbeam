9759f7c19fcc75ec65b5db5c3962a07077714644
Ryo Onodera <ryoqun@gmail.com>
2022-07-03 21:26:09 +0900

    save

--- crossbeam-deque/src/deque.rs
+++ crossbeam-deque/src/deque.rs
@@ -31,9 +31,9 @@ struct Buffer<T> {
     cap: usize,
 }
 
-unsafe impl<T, C> Send for Buffer<T> {}
+unsafe impl<T> Send for Buffer<T> {}
 
-impl<T, C> Buffer<T> {
+impl<T> Buffer<T> {
     /// Allocates a new buffer with the specified capacity.
     fn alloc(cap: usize) -> Buffer<T> {
         debug_assert_eq!(cap, cap.next_power_of_two());
