bfe46b5918594c8edd8b9cbe6d341bc9ec1e74a6
Ryo Onodera <ryoqun@gmail.com>
2022-07-04 12:27:02 +0900

    save

--- crossbeam-epoch/src/default.rs
+++ crossbeam-epoch/src/default.rs
@@ -45,7 +45,7 @@ pub trait CustomCollector {
     /// hhhh
     fn handle() -> &'static std::thread::LocalKey<LocalHandle>; 
 
-    fn new() -> dyn DynCustomCollector;
+    fn new() -> Box<dyn DynCustomCollector>;
 
     /// wwww
     fn with_handle<F, R>(mut f: F) -> R
