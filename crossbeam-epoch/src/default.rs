eb08154b44b25f8b48e07abe3edcd526dc5b7241
Ryo Onodera <ryoqun@gmail.com>
2022-07-04 12:09:46 +0900

    save

--- crossbeam-epoch/src/default.rs
+++ crossbeam-epoch/src/default.rs
@@ -45,7 +45,7 @@ pub trait CustomCollector {
     /// hhhh
     fn handle() -> &'static std::thread::LocalKey<LocalHandle>; 
 
-    fn new() -> Self {
+    fn new() -> Self where Self: Sized {
         Self
     }
 
