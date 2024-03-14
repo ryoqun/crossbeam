e76144c46e67050eec18a2c88c8fa6f67f58e3d9
Ryo Onodera <ryoqun@gmail.com>
2024-03-14 06:25:34 +0000

    save

--- crossbeam-channel/benchmarks/crossbeam-channel.rs
+++ crossbeam-channel/benchmarks/crossbeam-channel.rs
@@ -49,7 +49,7 @@ fn mpsc(cap: Option<usize>) {
     crossbeam::scope(|scope| {
         for _ in 0..THREADS {
             let core_id = core_id();
-            scope.spawn(move |_| {
+            scope.spawn(|_| {
             set_for_current(core_id);
                 for i in 0..MESSAGES / THREADS {
                     tx.send(message::new(i)).unwrap();
